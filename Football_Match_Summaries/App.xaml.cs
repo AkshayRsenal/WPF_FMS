a3b72adb7ca8573a92d01f4a3e8defc07e032b08
Akshay-Raul <raulakys@yahoo.com>
2020-02-02 01:41:46 +0100

    WPF App 1

---
+++ Football_Match_Summaries/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Football_Match_Summaries
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //public static ObservableCollection<Member> _members;
+
+        /*private ObservableCollection<Member> GenerateMembers(int cnt)
+        {
+            var obvlist = new ObservableCollection<Member>();
+
+            for (int i = 0; i < cnt; i++)
+            {
+                obvlist.Add(new Member { firstName = "fn" + i, lastName = "ln" + i });
+            }
+
+            return obvlist;
+        }*/
+
+        private string _firstName = "Akshay";   
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                /* _firstName = value;
+                 NotifyOfPropertyChange(() => FirstName);*/
+                _firstName = value;
+
+            }
+        }
+    }
+}
---
+++ Football_Match_Summaries/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+/*using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;*/
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace Football_Match_Summaries
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            BoundNumber = 0;
+
+        }
+
+        private int _boundNumber;
+        public int BoundNumber
+        {
+            get { return _boundNumber; }
+            set
+            {
+                if (_boundNumber != value)
+                {
+                    _boundNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
